ab520935ce5f1f08efaa7e12dda65097ed373137
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-05-19 21:55:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
+import { useState } from 'react';
 import './App.css';
-import {useState} from 'react';
+import { Task } from './Task';
+
 
 function App() {
-  // const age =10;
-  const [age, setAge] = useState(1);
 
-  const increaseAge = () => {
-    setAge(age + 1);
-};
+    const [todoList, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
+    // const [textColor, setTextColor] = useState("black")
 
+    const handleChange = (event) => {
+        setNewTask(event.target.value);
+    }
+    const addTask = () => {
+      const task = 
+      { 
+        id: todoList.length === 0 ? 1 : todoList[todoList.length-1].id + 1 ,
+        taskName : newTask,
+        isComplete : false,
+      }
+        setTodoList([...todoList, task])
+    }
+
+    const deleteTask = (id) => {
+      setTodoList(todoList.filter((task) =>  task.id !== id));
+    }
+    const markComplete = (id) => {
+      setTodoList(
+        todoList.map((task) => {
+          if ( task.id === id) 
+            return {...task, isComplete:true};
+          else 
+            return task;
+        })
+      )
+    }
   return ( 
     <div className="App"> 
-      {age} <button onClick={increaseAge}>Increase Age</button>
+      <div className='AddTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='List'>
+        {todoList.map((task) => {
+            return (
+              <Task 
+                taskName={task.taskName}
+                id={task.id}
+                isComplete={task.isComplete} 
+                deleteTask={deleteTask}
+                markComplete={markComplete}
+              />
+            )
+        })}
+      </div>
     </div> 
   );
 }

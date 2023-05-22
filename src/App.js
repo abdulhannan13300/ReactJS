a1102cdecc67d3c92c7ac12246231fc30e015d1b
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-05-22 11:00:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import {useState} from 'react';
 
 function App() {
 
-  const [showText, setShowText] = useState(true)
+const [textColor, setTextColor] = useState("black");
 
   return ( 
     <div className="App"> 
-    <button onClick={() => {
-      setShowText(!showText);    
-      }}>
-      Show/Hide
-    </button>
-    {showText && <h1>Hi I am Abdul Hannan</h1>}
+      <button onClick={() => {
+        setTextColor(textColor ==="black" ? "red" : "black");
+      }}>Change Text Color</button>
+      <h1 style={{color:textColor}}>This is the text for testing</h1>
+
     </div> 
   );
 }

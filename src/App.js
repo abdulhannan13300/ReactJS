525e3773b7078f25900ff62d442095b90e5622a8
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-05-27 16:09:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
-
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [catFact, setCatFact] = useState("")
+  const [data, setDatA] = useState(null)
+  const [name, setName] = useState("")
+  
 
-  const fetchCatFact = () => {
-    axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
-    });
+  const predictAge = () => {
+    axios.get(`https://api.agify.io/?name=${name}`)
+    .then((res) => {
+    setData(res.data)
+  })
   }
-
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
-
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}>Generate Cat Fact</button>
-      <p> {catFact} </p>
+      <input 
+        placeholder='Ex. Abdul Hannan'
+        onChange={(event) => {
+        setName(event.target.value);
+        }}  
+      />
+      <button onClick={predictAge}> Predict Age </button>
+      {/* When Using null in useState pls call using:<h2> Name: {data?.name}</h2>
+      or just use an object in useState({}) */}
+      <h2> Name: {data?.name}</h2>
+      <h2> Predicted age: {data?.age}</h2>
+      <h2> Count: {data?.count}</h2>
     </div>
   );
 }
